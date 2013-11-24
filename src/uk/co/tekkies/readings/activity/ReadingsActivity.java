8e42f95bc7843b7b7ff727edbc2ebbfa0bc176f4
Andy Joiner <andy.joiner.git@tekkies.co.uk>
2013-11-24 23:12:16 +0000

    WIP: Downloads some toast.

--- src/uk/co/tekkies/readings/activity/ReadingsActivity.java
+++ src/uk/co/tekkies/readings/activity/ReadingsActivity.java
@@ -16,6 +16,10 @@ limitations under the License.
 
 package uk.co.tekkies.readings.activity;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -28,6 +32,7 @@ import uk.co.tekkies.readings.fragment.ReadingsFragment;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.DatePickerDialog.OnDateSetListener;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -35,6 +40,8 @@ import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
@@ -50,6 +57,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class ReadingsActivity extends FragmentActivity implements OnDateSetListener, OnClickListener {
 
@@ -62,10 +70,13 @@ public class ReadingsActivity extends FragmentActivity implements OnDateSetListe
     PagerAdapter pagerAdapter;
     ViewPager viewPager;
     Boolean today = true;
+    static ReadingsActivity readingsActivity; 
+    
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        readingsActivity = this;
         dayDateFormat = new SimpleDateFormat("E");
         thisYearDateFormat = new SimpleDateFormat("E d MMM");
         anotherYearDateFormat = new SimpleDateFormat("E d MMM yy");
@@ -76,8 +87,63 @@ public class ReadingsActivity extends FragmentActivity implements OnDateSetListe
         if (!loadInstanceState(savedInstanceState)) {
             setDate(Calendar.getInstance());
         }
+        showNewsToast();
+    }
+    
+    private static Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            //((Passage)msg.obj).setSummary("downloaded");
+            //stringValues.set(index, msg.what+"|eek");
+            //int index = stringValues.indexOf(msg.obj);
+            //stringValues.set(index, msg.what+"|eek");
+            Toast.makeText(readingsActivity, msg.obj.toString(), Toast.LENGTH_LONG).show();
+//            notifyDataSetChanged();
+        }
+    };
+
+   
+    private void showNewsToast() {
+        Thread thread = new Thread(new Runnable(){
+            final String parameter="dummy";
+            @Override
+            public void run(){
+                String summary=downloadSummaryBackground(parameter);
+                Message message = Message.obtain(handler, 0, summary);
+                handler.sendMessage(message);
+            }
+          });
+         thread.setName("DownloadSummary");
+         thread.start();
+    }
+    
+    private String downloadSummaryBackground(String parameter2) {
+        String summary=null;
+        
+        URL url;
+        try {
+            String summaryUrl = "http://tekkies.co.uk/readings/summary/genesis1.txt";
+            url = new URL(summaryUrl);
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line = reader.readLine();
+            summary = "";
+            while( line != null)
+            {
+                summary += line;
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return summary;
     }
 
+    
+    
     private void setDate(Calendar calendar) {
         Log.v("DATE", "setDate(" + anotherYearDateFormat.format(new Date(calendar.getTimeInMillis())));
         centerCalendar = calendar;
