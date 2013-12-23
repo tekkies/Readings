43e7bc14bb546d2222b782a6d887d8d92350b851
Andy Joiner <andy.joiner.git@tekkies.co.uk>
2013-12-23 09:43:29 +0000

    Radio button works and selection is saved to prefs.

--- src/uk/co/tekkies/readings/adapter/Mp3ContentArrayAdapter.java
+++ src/uk/co/tekkies/readings/adapter/Mp3ContentArrayAdapter.java
@@ -18,81 +18,67 @@ package uk.co.tekkies.readings.adapter;
 
 import java.util.ArrayList;
 import uk.co.tekkies.readings.R;
-import uk.co.tekkies.readings.ReadingsApplication;
 import uk.co.tekkies.readings.activity.ContentLocationActivity;
-import uk.co.tekkies.readings.fragment.ReadingsFragment;
 import uk.co.tekkies.readings.model.Prefs;
 import uk.co.tekkies.readings.model.content.Mp3ContentLocator;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class Mp3ContentArrayAdapter extends ArrayAdapter<Mp3ContentLocator> implements OnClickListener {
 
     private ContentLocationActivity activity;
     private ArrayList<Mp3ContentLocator> mp3ContentLocators;
-    float defaultTextSize=0;
-    Prefs prefs;
+    private Prefs prefs;
+    private String selected="";
     
     public Mp3ContentArrayAdapter(Activity activity, ArrayList<Mp3ContentLocator> values) {
         super(activity, R.layout.listitem_portion, values);
         this.activity = (ContentLocationActivity) activity;
         this.mp3ContentLocators = values;
         prefs = new Prefs(activity);
+        selected = prefs.loadMp3Product();
     }
     
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.listitem_mp3_content, parent, false);
-        TextView textViewPassageTitle = (TextView) view.findViewById(R.id.passage_title);
-        TextView textViewSummary = (TextView) view.findViewById(R.id.textViewSummary);
-        if(defaultTextSize == 0) {
-            defaultTextSize = textViewSummary.getTextSize();
-        }
-        textViewSummary.setTextSize((float) (prefs.loadPassageTextSize() * defaultTextSize));
-        textViewPassageTitle.setText(mp3ContentLocators.get(position).getTitle());
-        view.setTag(mp3ContentLocators.get(position).getClass().getName());
-        
-        //textViewPassageTitle.setOnClickListener(this);
-        //textViewSummary.setOnClickListener(this);
-        
-        if (mp3ContentLocators.get(position).getBasePath().length() != 0) {
-            View listenView = view.findViewById(R.id.imageListen);
-            listenView.setVisibility(View.VISIBLE);
-            listenView.setOnClickListener(this);
-        }
-        textViewSummary.setText(mp3ContentLocators.get(position).getBasePath());
+    	//No recycling yet - still a very small list.
+        ViewGroup view = (ViewGroup) ((LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.listitem_mp3_content, parent, false);
+        Boolean contentFound = mp3ContentLocators.get(position).getBasePath().length() != 0;
+        //RadioButton
+        RadioButton radioButton = (RadioButton) view.findViewById(R.id.radioButton1);
+        radioButton.setChecked(mp3ContentLocators.get(position).getClass().getName().equals(selected));
+        radioButton.setTag(mp3ContentLocators.get(position).getClass().getName());
+        radioButton.setOnClickListener(this);
+        radioButton.setVisibility(contentFound ? View.VISIBLE : View.GONE);
+        //Listen
+        View listenView = view.findViewById(R.id.imageListen);
+        listenView.setOnClickListener(this);
+        listenView.setVisibility(contentFound ? View.VISIBLE : View.GONE);
+        //Text
+        ((TextView)view.findViewById(R.id.passage_title)).setText(mp3ContentLocators.get(position).getTitle());
+        ((TextView) view.findViewById(R.id.textViewSummary)).setText(mp3ContentLocators.get(position).getBasePath());
         return view;
     }
 
     public void onClick(View v) {
-//        switch (v.getId()) {
-//        case R.id.passage_title:
-//            tryOpenIntegratedReader(((View) v.getParent().getParent()).getTag().toString());
-//            break;
-//        case R.id.textViewSummary:
-//            tryOpenIntegratedReader(((View) v.getParent()).getTag().toString());
-//            break;
-//        case R.id.imageViewReadOffline:
-//            tryOpenIntegratedReader(((View) v.getParent().getParent()).getTag().toString());
-//            break;
-//        case R.id.imageListen:
-//            openMp3(((View) v.getParent().getParent()).getTag().toString());
-//            break;
-//        case R.id.imageViewReadOnline:
-//            openPositiveWord(((View) v.getParent().getParent()).getTag().toString());
-//            // openIntegratedReader(((View)v.getParent().getParent()).getTag().toString());
-//            break;
-//        }
+        switch (v.getId()) {
+        case R.id.radioButton1:
+        	doRadioButtonClick(v);
+        break;
+        }
     }
-    
+
+	private void doRadioButtonClick(View v) {
+		selected = v.getTag().toString();
+		prefs.saveMp3Product(selected);
+		this.notifyDataSetChanged();
+	}
 }
