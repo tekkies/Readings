0a2e19cf33c20b66a35d3d9f493f8c852cf136af
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-11-02 23:33:18 +0000

    Reassembled project in Android Studio gradle form.

---
+++ app/src/androidTest/java/tekkies/co/uk/readings/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tekkies.co.uk.readings;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
