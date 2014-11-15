54f9c516e68a1c99603af40b7181fd9724c303c5
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-11-15 14:17:56 +0000

    Four UI tests now.

--- app/src/androidTest/java/uk/co/tekkies/readings/test/bdd/readingsActivity/ReadingsActivityTest.java
+++ app/src/androidTest/java/uk/co/tekkies/readings/test/bdd/readingsActivity/ReadingsActivityTest.java
@@ -10,6 +10,8 @@ import uk.co.tekkies.readings.test.Utils;
 
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.swipeLeft;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.swipeRight;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
@@ -29,22 +31,48 @@ public class ReadingsActivityTest extends ActivityInstrumentationTestCase2<Readi
     }
 
     public void testOpenSpecificDate() {
-        final ReadingsActivity readingsActivity = getActivity();
-        readingsActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                readingsActivity.onDateSet(null, 2011, Calendar.FEBRUARY, 1);
-            }
-        });
+        jumpToToday();
+        jumpTo(2011, Calendar.FEBRUARY, 1);
         onView(withText(containsString("Exodus 5"))).check(matches(isDisplayed()));
         onView(withText(containsString("Tue 1 Feb 11"))).check(matches(isDisplayed()));
     }
 
     public void testOpenToday() {
+        jumpTo(2011, Calendar.MARCH, 20);
         onView(withId(R.id.menu_date)).perform(click());
         onView(withText("Done")).perform(click());
         onView(withText(containsString("Today"))).check(matches(isDisplayed()));
     }
 
+    public void testSwipeRight() {
+        jumpTo(2011, Calendar.FEBRUARY, 1);
+        onView(withText(containsString("Exodus 5"))).perform(swipeRight());
+        onView(withText(containsString("Exodus 3"))).check(matches(isDisplayed()));
+        onView(withText(containsString("Mon 31 Jan 11"))).check(matches(isDisplayed()));
+    }
+
+    public void testSwipeLeft() {
+        jumpTo(2011, Calendar.FEBRUARY, 1);
+        onView(withText(containsString("Exodus 5"))).perform(swipeLeft());
+        onView(withText(containsString("Exodus 7"))).check(matches(isDisplayed()));
+        onView(withText(containsString("Wed 2 Feb 11"))).check(matches(isDisplayed()));
+    }
+
+
+    private void jumpTo(final int year, final int month, final int day) {
+        final ReadingsActivity readingsActivity = getActivity();
+        readingsActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                readingsActivity.onDateSet(null, year, month, day);
+            }
+        });
+    }
+
+    private void jumpToToday() {
+        onView(withId(R.id.menu_date)).perform(click());
+        onView(withText("Done")).perform(click());
+        onView(withText(containsString("Today"))).check(matches(isDisplayed()));
+    }
 
 }
--- app/src/main/java/uk/co/tekkies/readings/Features.java
+++ app/src/main/java/uk/co/tekkies/readings/Features.java
@@ -1,5 +1,5 @@
 package uk.co.tekkies.readings;
 
 public class Features {
-    public static final boolean DAILYREADINGS_ORG_COMMENTS=true;
+    public static final boolean DAILYREADINGS_ORG_COMMENTS=false;
 }
