6b3d2c664fe54b16915dc511bb5452458a1f1147
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-11-16 18:32:01 +0000

    v3.3.2

--- in.bat
+++ in.bat
@@ -1,3 +1,3 @@
-call adb install -r "Apk\Readings.Signed.apk"
+call adb install -r "Apk\app-release.apk"
 adb shell am start -n uk.co.tekkies.readings/uk.co.tekkies.readings.activity.ReadingsActivity 
 pause
