a39106ee05f7a60c28bd09458c5981342aded4be
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-11-16 12:36:02 +0000

    Don't use framework in test setup.

--- app/src/androidTest/java/uk/co/tekkies/readings/test/bdd/readingsActivity/ReadingsActivityTestBase.java
+++ app/src/androidTest/java/uk/co/tekkies/readings/test/bdd/readingsActivity/ReadingsActivityTestBase.java
@@ -2,12 +2,14 @@ package uk.co.tekkies.readings.test.bdd.readingsActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
 import android.preference.PreferenceManager;
 import android.test.ActivityInstrumentationTestCase2;
 
 import uk.co.tekkies.readings.R;
 import uk.co.tekkies.readings.activity.ReadingsActivity;
 import uk.co.tekkies.readings.model.Prefs;
+import uk.co.tekkies.readings.util.Analytics;
 
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
@@ -38,8 +40,15 @@ public class ReadingsActivityTestBase extends ActivityInstrumentationTestCase2<R
     }
 
     public static void suppressStartupWhatsNew(Context context) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        new Prefs(context).checkForUpgrade(); //Checks and updates
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        try {
+            PackageInfo pi = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt(Prefs.VERSION_KEY, pi.versionCode);
+            editor.commit();
+        } catch (Exception e) {
+            Analytics.reportCaughtException(context, e);
+        }
     }
 
 }
