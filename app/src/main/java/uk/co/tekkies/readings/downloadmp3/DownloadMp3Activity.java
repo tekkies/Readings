1ab58452202f525a06226913182ef6643f0c7994
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-11-21 22:13:02 +0000

    WIP: Button to start download.

--- app/src/main/java/uk/co/tekkies/readings/downloadmp3/DownloadMp3Activity.java
+++ app/src/main/java/uk/co/tekkies/readings/downloadmp3/DownloadMp3Activity.java
@@ -1,14 +1,18 @@
 package uk.co.tekkies.readings.downloadmp3;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 
 import uk.co.tekkies.readings.R;
+import uk.co.tekkies.readings.util.Analytics;
 
-public class DownloadMp3Activity extends Activity {
+public class DownloadMp3Activity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,7 @@ public class DownloadMp3Activity extends Activity {
         setContentView(R.layout.activity_download_mp3);
         WebView webView = (WebView) findViewById(R.id.webView);
         webView.loadUrl("http://server.firefighters.org/kjv.asp");
+        findViewById(R.id.buttonDownload).setOnClickListener(this);
     }
 
 
@@ -39,5 +44,19 @@ public class DownloadMp3Activity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonDownload:
+                doDownloadZip();
+                break;
+        }
+    }
+
+    private void doDownloadZip() {
+        Analytics.UIClick(this, "download-mp3");
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://server.firefighters.org/kjv/kjv.zip")));
     }
 }
