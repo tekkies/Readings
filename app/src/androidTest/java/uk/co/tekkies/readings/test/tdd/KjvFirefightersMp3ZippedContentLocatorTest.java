2c424670259b83efc7cb0311010f7af71d05210e
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-11-29 23:20:55 +0000

    WIP: Look at test setup for mp3 locating.

--- app/src/androidTest/java/uk/co/tekkies/readings/test/tdd/KjvFirefightersMp3ZippedContentLocatorTest.java
+++ app/src/androidTest/java/uk/co/tekkies/readings/test/tdd/KjvFirefightersMp3ZippedContentLocatorTest.java
@@ -2,12 +2,22 @@ package uk.co.tekkies.readings.test.tdd;
 
 import junit.framework.TestCase;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import uk.co.tekkies.readings.model.content.KjvFirefightersMp3ZippedContentLocator;
 
-/**
- * Created by ajoiner on 03/11/2014.
- */
 public class KjvFirefightersMp3ZippedContentLocatorTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+//        InputStream inputStream;
+//        try {
+//            inputStream = context.getAssets().open("sample.mp3");
+//        } catch (IOException e) {
+//        }
+    }
 
     public void testConfirmKeyFileFound() {
         KjvFirefightersMp3ZippedContentLocator locator=new KjvFirefightersMp3ZippedContentLocator();
