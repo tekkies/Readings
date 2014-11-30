c0f94826c6a04f255fd0424b7ba8ca9fe4a5ecdf
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-11-30 22:49:49 +0000

    Remove previous summary update.

--- in.bat
+++ in.bat
@@ -1,3 +1,3 @@
-call adb install -r "Apk\app-release.apk"
+call adb install -r "app\build\outputs\apk\app-debug-unaligned.apk"
 adb shell am start -n uk.co.tekkies.readings/uk.co.tekkies.readings.activity.ReadingsActivity 
 pause
