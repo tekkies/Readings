f975a8f17f47c7c4f1801f1fc2b564ec220a13f2
Andy Joiner <andy.joiner@tekkies.co.uk>
2015-09-05 23:48:58 +0100

    Set device to landscape in test.bat
    #risk2 #bdd
    
    Set device to landscape, so we don't have to guess what actions are in the overflow menu (note: using nexus7 device)

--- test.bat
+++ test.bat
@@ -1,1 +1,6 @@
-adb shell am instrument -w uk.co.tekkies.readings.test/com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner
+REM Turn off auto-rotation
+adb shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0
+REM Rotate to landscape
+adb shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:1
+REM Test
+adb shell am instrument -w uk.co.tekkies.readings.test/com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner | tee espresso-results.txt
