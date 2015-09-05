b6492258760ceb1ec0b75d2e5e7028b2c406efbd
Andy Joiner <andy.joiner@tekkies.co.uk>
2015-09-06 00:11:29 +0100

    Run tdd and bdd from gradle
    #risk2 #bdd #tdd
    
    HTML reports generated in app\build\reports

--- test.bat
+++ test.bat
@@ -1,6 +1,12 @@
+REM uninstall before test
+call un
 REM Turn off auto-rotation
 adb shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0
 REM Rotate to landscape
 adb shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:1
-REM Test
-adb shell am instrument -w uk.co.tekkies.readings.test/com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner | tee espresso-results.txt
+REM TDD
+call gradlew :App:testDebug
+if %errorlevel% neq 0 exit /b %errorlevel%
+REM BDD
+call gradlew app:connectedCheck
+if %errorlevel% neq 0 exit /b %errorlevel%
